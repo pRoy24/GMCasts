c04e82c81624085f6f012e8129bedc7c92f639e2
proy24 <roypritam1234@gmail.com>
2024-03-24 00:51:47 +0700

    updates

--- pages/frame_page/init/[id].js
+++ pages/frame_page/init/[id].js
@@ -44,11 +44,6 @@ export default function InitFramePage(props) {
 
 export async function getStaticPaths() {
   const paths = [
-    {
-      params: {
-        id: 'b390oneg0u2wq3fd'
-      }
-    }
   ]
   return {
     paths: paths,
