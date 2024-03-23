0310b0120b49add13862e02c0b4e21d897d456ee
proy24 <roypritam1234@gmail.com>
2024-03-24 03:03:49 +0700

    updates

--- pages/api/create_cast.js
+++ pages/api/create_cast.js
@@ -1,4 +1,5 @@
-
+import axios from 'axios';
+const STREAMER_SERVER = process.env.STREAMER_SERVER;
 
 const {
   makeCastAdd,
@@ -15,6 +16,7 @@ const {
 
 const { hexToBytes } = require("@noble/hashes/utils");
 
+
 const ACCOUNT_PRIVATE_KEY = process.env.SIGNER_PRIVATE_KEY;
 
 const FID = 402685; // Your fid
@@ -30,34 +32,11 @@ export default async function handler(req, res) {
 
   const payload = req.body;
 
-  const frameURL = `${FRAME_BASE_URL}/${payload.playbackId}`
+  await axios.post(`${STREAMER_SERVER}/hub/cast`, payload);
 
-  const dataOptions = {
-    fid: FID,
-    network: FarcasterNetwork.MAINNET,
-  };
+  
 
-
-  const castAddReq = await makeCastAdd(
-    {
-      text: `${frameURL}`,
-      embeds: [],
-      embedsDeprecated: [],
-      mentions: [],
-      mentionsPositions: [],
-    },
-    dataOptions,
-    ed25519Signer,
-  );
-
-
-  const cast = castAddReq._unsafeUnwrap();
-
-
-  const messageResponse = await hubClient.submitMessage(cast);
-
-
-  return (messageResponse);
-
+  res.status(200).json({ message: 'Hello from the server!' })
+  
 
 }
